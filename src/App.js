9b8ba00c866cadd77c46720f3d6046423c809dad
LeeEungjin <dmdwls0208@gmail.com>
2020-03-21 20:56:12 +0900

    #3.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
 import React from "react";
-import PropType from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0
-  };
-
-  /*
-    setState 함수를 실행하면 render 함수를 실행해서 state 값만 변경시켜준다.
-    state 값을 변경할 때는 curent 을 사용하여 변경!
-    (this.state.count + 1 처럼 직접 state 값을 변경하지 않도록 주의!)
-  */
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
+    isLoading: true
   };
 
   componentDidMount() {
-    console.log("Mounting!");
+    setTimeout(() => {
+      this.setState(current => ({ isLoading: !current.isLoading }));
+    }, 3000);
   }
-
-  componentDidUpdate() {
-    console.log("Updateing");
-  }
-
-  componentWillUnmount() {
-    console.log("Goodbye!");
-  }
-
   render() {
-    console.log("rendering!");
-    return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading.." : "We are ready"}</div>;
   }
 }
 
