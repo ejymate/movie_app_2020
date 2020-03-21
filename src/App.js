53e898a85167893a871e30d6060ec692f2e12fc0
LeeEungjin <dmdwls0208@gmail.com>
2020-03-21 20:25:42 +0900

    #3.0 Class Component and State

--- src/App.js
+++ src/App.js
@@ -1,74 +1,33 @@
 import React from "react";
 import PropType from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.8
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 5.5
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.7
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  /*
+    setState 함수를 실행하면 render 함수를 실행해서 state 값만 변경시켜준다.
+    state 값을 변경할 때는 curent 을 사용하여 변경!
+    (this.state.count + 1 처럼 직접 state 값을 변경하지 않도록 주의!)
+  */
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
-];
-
-Food.propType = {
-  name: PropType.string.isRequried,
-  picture: PropType.string.isRequried,
-  rating: PropType.number
-};
-
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h1>I love {name}</h1>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}></img>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-      {foodILike.map(dish => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
 }
 
 export default App;
